28699cfbb7ab7b68898f439597299b9ecd5b0c5a
Giovanni Ferro <giovanniferro5@gmail.com>
2022-10-08 10:58:16 -0400

    exercicio3 adicionado

---
+++ exercicio3-area-sala.py
@@ -0,0 +1,9 @@
+# Escreva um algoritmo que peça ao usuário para entrar com a largura e comprimento
+# de uma sala. Uma vez que os valores são lidos, mostre na tela a área da sala.
+
+largura = float(input("digite a largura da sala: "))
+altura = float(input("digite a altura da sala: "))
+
+area = altura * largura
+
+print(f"A sala tem {area} de área.")

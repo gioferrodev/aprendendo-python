4ebd567dd5b4f35eb82d4ce11e68da9b911add59
Giovanni Ferro <giovanniferro5@gmail.com>
2022-10-08 10:49:27 -0400

    exercicio2-ola.py modificado

--- exercicio2-ola.py
+++ exercicio2-ola.py
@@ -1,4 +1,4 @@
-# Crie um algoritmo que pergunte o nome do usuário e mostre uma saudadação para
+# Crie um algoritmo que pergunte o nome do usuário e mostre uma saudação para
 #  ele com seu nome.
 nome = input("Por favor, digite seu nome: ")
 print(f"Olá {nome}, seja bem vindo!")

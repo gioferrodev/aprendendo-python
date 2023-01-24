248afb735f124fe12e486b6c70d91b06ac93ddfa
Giovanni Ferro <giovanniferro5@gmail.com>
2023-01-24 15:00:19 -0400

    exercicio 15 adicionado

---
+++ exercicio15-ano-zodiaco.py
@@ -0,0 +1,24 @@
+# O horóscopo é comumente representado usando a posição do sol em relação
+# ao nascimento da pessoa. Este sistema astrológico divide o ano em 12 signos
+# do zodíaco como é mostrado na tabela abaixo.
+# =============================================
+# Signo do zodíaco || Data
+# =============================================
+# Capricórnio   ||  22/Dezembro a 29/Janeiro
+# Aquário   ||  20/Janeiro a 18/Fevereiro
+# Peixes    ||  19/Fevereiro a 20/Março
+# Áries     ||  21/Março a 19/Abril
+# Touro     || 20/Abril a 20/Maio
+# Gêmeos    || 21/Maio a 20/Junho
+# Câncer    ||  21/Junho a 22/Junho
+# Leão      || 23/Julho a 23/Agosto
+# Virgem    || 23/Agosto a 22/Setembro
+# Libra     ||  23/Setembro a 22/Outubro
+# Escorpião || 23/Outubro a 21/Novembro
+# Sagitário ||  22/Novembro a 21/Dezembro
+
+# Escreva um programa em Python que peça ao usuário o dia e mês em que ele nasceu.
+# O programa deve mostrar uma msg informando o seu signo
+
+dia = int(input('digite o seu dia de nascimento: '))
+mes = input('digite agora o seu mês de nascimento: ')

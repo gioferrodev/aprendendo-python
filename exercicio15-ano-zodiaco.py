41834c4822f6e31a7e3b6c2e845d5dee32d8aea7
Giovanni Ferro <giovanniferro5@gmail.com>
2023-02-07 17:15:23 -0400

    exercicio modificado

--- exercicio15-ano-zodiaco.py
+++ exercicio15-ano-zodiaco.py
@@ -4,7 +4,7 @@
 # =============================================
 # Signo do zodíaco || Data
 # =============================================
-# Capricórnio   ||  22/Dezembro a 29/Janeiro
+# Capricórnio   ||  22/Dezembro a 19/Janeiro
 # Aquário   ||  20/Janeiro a 18/Fevereiro
 # Peixes    ||  19/Fevereiro a 20/Março
 # Áries     ||  21/Março a 19/Abril
@@ -19,15 +19,19 @@
 
 # Escreva um programa em Python que peça ao usuário o dia e mês em que ele nasceu.
 # O programa deve mostrar uma msg informando o seu signo
+print('\n','*'*10, 'CALCULADORA DO ZODÍACO', '*'*10,'\n')
 
 dia = int(input('digite o seu dia de nascimento: '))
-mes = input('digite agora o seu mês de nascimento: ')
+mes = input('digite por extenso o mês do seu aniversário: ')
 
-if mes == 'janeiro' and dia in range(20, 32) or mes == 'fevereiro' and dia in range(1, 19):
+
+if mes == 'dezembro' and dia in range(22,31) or mes == 'janeiro' and dia in range(1,20):
+    print('Você é do signo de capricórnio.')
+elif mes == 'janeiro' and dia in range(20, 32) or mes == 'fevereiro' and dia in range(1, 19):
     print('Você é do signo de aquário.')
 elif mes == 'feveiro' and dia in range(19, 29) or mes == 'março' and dia in range(1, 21):
-    print('Você é signo de peixes.')
+    print('Você é do signo de peixes.')
 elif mes == 'março' and dia in range(21, 32) or mes == 'abril' and dia in range(1, 20):
-    print('Você é signo de peixes.')
+    print('Você é do signo de áries.')
 else:
     print('Data inválida')
